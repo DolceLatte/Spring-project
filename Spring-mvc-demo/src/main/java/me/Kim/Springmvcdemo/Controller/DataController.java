229629601afd64c3b93dc017dbcd00eed91f8bd3
201502038 <bnm961126@gmail.com>
2019-09-03 17:27:08 +0900

    add controller handler

--- Spring-mvc-demo/src/main/java/me/Kim/Springmvcdemo/Controller/DataController.java
+++ Spring-mvc-demo/src/main/java/me/Kim/Springmvcdemo/Controller/DataController.java
@@ -1,13 +1,34 @@
 package me.Kim.Springmvcdemo.Controller;
 
+import jdk.nashorn.internal.parser.JSONParser;
+import me.Kim.Springmvcdemo.Domain.Data;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class DataController {
 
-    @GetMapping("/get")
-    public String get(){
-        return "Get";
+    @GetMapping(value = "/get/{key}")
+    @ResponseBody
+    public String get(@PathVariable String key){
+        return key;
     }
+
+    @PostMapping(value = "/post",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ResponseBody
+    public Data post(Data data){
+        System.out.println(data);
+        return data;
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public void delete(){
+        return;
+    }
+
+
 }
---
+++ Spring-mvc-demo/src/main/java/me/Kim/Springmvcdemo/Domain/Data.java
@@ -0,0 +1,11 @@
+package me.Kim.Springmvcdemo.Domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @Builder
+public class Data {
+
+    private int number;
+}
